81e18888a04393d493d664d331e60ce700371215
Manuel Zambrano <mmzmm3z@gmail.com>
2019-03-09 00:07:14 -0300

    Nueva fuente

---
+++ precache-manifest.07059f17c5f0074db745b83664c738bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e5fe2e4c9123c55328875fb756056d9",
+    "url": "/static/media/profile.1e5fe2e4.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "1fb2acdb78bddd6a32e7",
+    "url": "/static/js/main.a6ca1378.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "1fb2acdb78bddd6a32e7",
+    "url": "/static/css/main.4f735d89.chunk.css"
+  },
+  {
+    "revision": "66dafeba2b455d0e0783ef0bad4768d4",
+    "url": "/index.html"
+  }
+];
